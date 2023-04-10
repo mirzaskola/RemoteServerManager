1c313e99c2c32f156b8cccb24fce5554255207c5
mirzaskola <Gigapujdoala4>
2023-04-10 19:13:02 +0200

    Created basic skeleton of welcome screen

--- app/src/main/java/com/example/remoteservermanager/MainActivity.kt
+++ app/src/main/java/com/example/remoteservermanager/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.remoteservermanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.remoteservermanager.ui.theme.RemoteServerManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    WelcomeScreen()
                 }
             }
         }
@@ -30,14 +37,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun WelcomeScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Column(modifier = modifier
+            .fillMaxWidth()
+            .background(Color.Blue)
+            .padding(vertical = 8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "REMOTE SERVER MANAGER")
+        }
+        Column(modifier = modifier.padding(8.dp)) {
+            Text(text = "List of active servers:")
+        }
+        Column(modifier = modifier
+            .fillMaxSize()
+            .fillMaxWidth()) {
+            Text(text = "SERVER 1", modifier = modifier.padding(start = 5.dp))
+            Text(text = "SERVER 2", modifier = modifier.padding(start = 5.dp))
+            Text(text = "SERVER 3", modifier = modifier.padding(start = 5.dp))
+            Text(text = "SERVER 4", modifier = modifier.padding(start = 5.dp))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     RemoteServerManagerTheme {
-        Greeting("Android")
+        WelcomeScreen()
     }
 }
